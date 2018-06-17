d2ad2133ad98fbdcd97e10a77f63642078ddaaee
timsully <timsullivan97@gmail.com>
2018-06-16 23:02:57 -0700

    created draw function with canvas in html

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>HTML5 Canvas</title>
-</head>
-<body>
-<canvas id="draw" width="800" height="800"></canvas>
-<script>
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
+
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 100;
-// ctx.globalCompositeOperation = 'multiply';
+ctx.globalCompositionOperation = 'multiply';
+
 
 let isDrawing = false;
 let lastX = 0;
@@ -23,33 +17,36 @@ let lastY = 0;
 let hue = 0;
 let direction = true;
 
+
 function draw(e) {
-  if (!isDrawing) return; // stop the fn from running when they are not moused down
+  if(!isDrawing) return; //stop function from running when mouse isn't down
+
   console.log(e);
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
-  // start from
+  // Start from
   ctx.moveTo(lastX, lastY);
-  // go to
+  // Go to
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
-
   hue++;
-  if (hue >= 360) {
+  if(hue >= 360) {
     hue = 0;
   }
+
+
   if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
     direction = !direction;
   }
-
-  if(direction) {
-    ctx.lineWidth++;
+  if (direction) {
+  ctx.lineWidth++;
   } else {
     ctx.lineWidth--;
   }
 
 }
+
 
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
@@ -57,17 +54,8 @@ canvas.addEventListener('mousedown', (e) => {
 });
 
 
+// Mouse event being logged
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
-
-</script>
-
-<style>
-  html, body {
-    margin:0;
-  }
-</style>
-
-</body>
-</html>
